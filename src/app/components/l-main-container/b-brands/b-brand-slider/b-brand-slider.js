3a8c4e3735d0fcf08c5c0287d339f84e1c4abfed
Cheder-one <cherbadenis1@gmail.com>
2023-09-25 02:36:40 +0300

    Отладка слайдера закончена

--- src/app/components/l-main-container/b-brands/b-brand-slider/b-brand-slider.js
+++ src/app/components/l-main-container/b-brands/b-brand-slider/b-brand-slider.js
@@ -1,38 +1,13 @@
 import Swiper from "swiper";
 import { Pagination } from "swiper/modules";
 
+import {
+  addHiddenBrands,
+  removeHiddenBrands
+} from "./utils/toggleHiddenBrands";
+
 let mobileSlider = null;
-
 const brandSlider = document.querySelector(".b-brand-slider");
-const sliderWrapper = document.querySelector(
-  ".b-brand-slider__e-wrapper"
-);
-const brandsTemplate = document.querySelector(
-  "#b-brand-slider__hidden-brands"
-);
-
-const findHiddenBrands = () => {
-  return document.querySelectorAll(
-    ".b-brand-slider__e-slide--hidden"
-  );
-};
-
-const addHiddenBrands = () => {
-  const hiddenBrands = findHiddenBrands();
-
-  if (hiddenBrands.length === 0) {
-    const clonedTemplate = brandsTemplate.content.cloneNode(true);
-    sliderWrapper.appendChild(clonedTemplate);
-  }
-};
-
-const removeHiddenBrands = () => {
-  const hiddenBrands = findHiddenBrands();
-
-  if (hiddenBrands.length > 0) {
-    hiddenBrands.forEach((el) => el.remove());
-  }
-};
 
 const toggleMobileSlider = () => {
   if (

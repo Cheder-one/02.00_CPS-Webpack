c2061190b35ac4b3ad1cebc2879d0fe6ea1a50b1
Cheder-one <cherbadenis1@gmail.com>
2023-09-24 03:28:57 +0300

    Внес коррективы по отступам

--- src/main.js
+++ src/main.js
@@ -2,5 +2,3 @@ import "./index.html";
 import "./styles/main.css";
 
 import "./app/components/l-main-container/l-main-container.js";
-
-const bBrandSlider = document.querySelector(".b-brand-slider");

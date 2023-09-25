1a68858803c7a5f61fc3db340be76a62dac22d23
Cheder-one <cherbadenis1@gmail.com>
2023-09-26 01:33:42 +0300

    develop восстановлен

--- src/app/components/l-main-container/b-brands/b-brand-slider/b-brand-slider.js
+++ src/app/components/l-main-container/b-brands/b-brand-slider/b-brand-slider.js
@@ -1,5 +1,5 @@
 import Swiper from "swiper";
-import { Keyboard, Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
 import {
   addHiddenBrands,
@@ -8,47 +8,44 @@ import {
 
 let mobileSlider = null;
 const brandSlider = document.querySelector(".b-brand-slider");
-const mediaQuerySmall = window.matchMedia("(max-width: 767.9px)");
 
-const createBrandSlider = () => {
-  return new Swiper(brandSlider, {
-    modules: [Pagination, Keyboard],
-
-    loop: true,
-    direction: "horizontal",
-    slidesPerView: "auto",
-    spaceBetween: "16px",
-
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-      pageUpDown: true
-    },
-
-    pagination: {
-      el: ".b-brand-slider__e-pagination",
-      type: "bullets",
-      clickable: true
-    }
-  });
-};
-
-function handleMediaChange(event) {
-  const isSmallScreen = event.matches;
-
-  if (isSmallScreen) {
+const toggleMobileSlider = () => {
+  if (
+    window.innerWidth <= 767.9 &&
+    brandSlider.dataset.mobile === "false"
+  ) {
     removeHiddenBrands();
-    mobileSlider = createBrandSlider();
+
+    mobileSlider = new Swiper(brandSlider, {
+      modules: [Pagination],
+
+      loop: true,
+      direction: "horizontal",
+      slidesPerView: 1.3,
+      spaceBetween: "16px",
+
+      pagination: {
+        el: ".b-brand-slider__e-pagination",
+        type: "bullets",
+        clickable: true
+      }
+    });
 
     brandSlider.dataset.mobile = "true";
-  } else {
+  }
+
+  if (window.innerWidth >= 768) {
+    brandSlider.dataset.mobile = "false";
+
     if (brandSlider.classList.contains("swiper-initialized")) {
       mobileSlider.destroy();
     }
     addHiddenBrands();
-    brandSlider.dataset.mobile = "false";
   }
-}
+};
 
-handleMediaChange(mediaQuerySmall);
-mediaQuerySmall.addEventListener("change", handleMediaChange);
+toggleMobileSlider();
+
+window.addEventListener("resize", () => {
+  toggleMobileSlider();
+});
